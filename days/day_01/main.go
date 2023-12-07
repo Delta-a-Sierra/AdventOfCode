f9938b52834d3042a8ad7ba58f7c542c1bb90273
Dwayne <dwayne@MacBook-Pro.local>
2023-12-07 09:32:33 -0500

    intial day 1 solution

--- days/day_01/main.go
+++ days/day_01/main.go
@@ -2,7 +2,19 @@ package day_01
 
 import (
 	"fmt"
+	"strconv"
+	"sync"
+	"unicode"
 )
+
+var numbers = map[string]map[string]string{
+	"o": {"one": "1"},
+	"t": {"two": "2", "three": "3"},
+	"f": {"four": "4", "five": "5"},
+	"s": {"six": "6", "seven": "7"},
+	"e": {"eight": "8"},
+	"n": {"nine": "9"},
+}
 
 // Run function of the daily challenge
 func Run(input []string, mode int) {
@@ -14,12 +26,87 @@ func Run(input []string, mode int) {
 	}
 }
 
+func isNumber(input string) (string, bool) {
+	if !unicode.IsLetter(rune(input[0])) {
+		return "", false
+	}
+
+	possibles := numbers[string(input[0])]
+	for k, num := range possibles {
+		if len(input) < len(k) {
+			continue
+		}
+		if input[:len(k)] == k {
+			return num, true
+		}
+	}
+	return "", false
+}
+
+func parseNum(input string) int {
+	wg := sync.WaitGroup{}
+
+	wg.Add(1)
+	first, second := "", ""
+	go func(input string) {
+		for i := 0; i < len(input); i++ {
+			if unicode.IsLetter(rune(input[i])) {
+				if num, ok := isNumber(input[i:]); ok {
+					first = num
+					break
+				}
+
+				continue
+
+			}
+
+			first = string(input[i])
+			break
+		}
+		wg.Done()
+	}(input)
+
+	wg.Add(1)
+	go func(input string) {
+		for i := len(input) - 1; i >= 0; i-- {
+			if unicode.IsLetter(rune(input[i])) {
+				if num, ok := isNumber(input[i:]); ok {
+					second = num
+					break
+				}
+				continue
+			}
+
+			second = string(input[i])
+			break
+		}
+		wg.Done()
+	}(input)
+
+	wg.Wait()
+
+	out := fmt.Sprintf("%s%s", first, second)
+	outI, _ := strconv.Atoi(out)
+
+	return outI
+}
+
 // Part1 solves the first part of the exercise
 func Part1(input []string) string {
-	return ""
+	res := 0
+
+	for _, line := range input {
+		res += parseNum(line)
+	}
+	return fmt.Sprintf("%d", res)
 }
 
 // Part2 solves the second part of the exercise
 func Part2(input []string) string {
-	return ""
+	res := 0
+
+	for _, line := range input {
+		res += parseNum(line)
+	}
+	return fmt.Sprintf("%d", res)
 }
